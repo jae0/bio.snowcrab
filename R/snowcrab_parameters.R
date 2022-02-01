164e2f61aed063951e93f1d675afedbe7651d181
Jae S Choi <choi.jae.seok@gmail.com>
2022-02-01 10:19:07 -0400

    tweak params

--- R/snowcrab_parameters.R
+++ R/snowcrab_parameters.R
@@ -443,9 +443,9 @@ snowcrab_parameters = function( p=list(), year.assessment=NULL, project_name="bi
         p$carstm_prediction_surface_parameters = parameters_add_without_overwriting( p$carstm_prediction_surface_parameters,
           bathymetry = aegis.bathymetry::bathymetry_parameters( project_class="stmv"  ),
           substrate = aegis.substrate::substrate_parameters(   project_class="stmv"  ),
-          temperature = aegis.temperature::temperature_parameters( project_class="carstm", carstm_model_label="1970_present", yrs=1970:p$year.assessment ),  # 1970_present has better spatial resolution
-          speciescomposition_pca1 = aegis.speciescomposition::speciescomposition_parameters(  project_class="carstm", carstm_model_label="1999_present", variabletomodel="pca1", yrs=1999:p$year.assessment ),
-          speciescomposition_pca2 = aegis.speciescomposition::speciescomposition_parameters(  project_class="carstm", carstm_model_label="1999_present", variabletomodel="pca2", yrs=1999:p$year.assessment )
+          temperature = aegis.temperature::temperature_parameters( project_class="carstm", carstm_model_label="1970_present" ),  # 1970_present has better spatial resolution
+          speciescomposition_pca1 = aegis.speciescomposition::speciescomposition_parameters(  project_class="carstm", carstm_model_label="1999_present", variabletomodel="pca1" ),
+          speciescomposition_pca2 = aegis.speciescomposition::speciescomposition_parameters(  project_class="carstm", carstm_model_label="1999_present", variabletomodel="pca2" )
         )
     }
 
