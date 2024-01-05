a721a270a9d6636b7582ab5e7e625f18d8d88377
Jae S Choi <choijaeseok@gmail.com>
2024-01-05 17:27:29 -0400

    ggplot

--- R/snowcrab_mapping_features.R
+++ R/snowcrab_mapping_features.R
@@ -1,5 +1,12 @@
 
-snowcrab_mapping_features = function( p, redo=FALSE ) {
+snowcrab_mapping_features = function( p, 
+  area_lines=NULL, 
+  isobaths=c( 100, 200, 300, 400, 500 ), 
+  coastline=c("canada", "united states of america"), 
+  xlim=c(-85,-35), ylim=c(35, 65),
+  redo=FALSE, target=c("ggplot", "tmap")  ) {
+
+    # same as carstm::features-to_add, but with different defaults
 
     fn = file.path( p$project.outputdir, "snowcrab_mapping_features.RDS" )
     if (!redo){
@@ -9,24 +16,44 @@ snowcrab_mapping_features = function( p, redo=FALSE ) {
     }
 
     plot_crs = p$aegis_proj4string_planar_km
-    z = aegis.bathymetry::isobath_db(  depths=c( 100, 200, 300, 400, 500 ), project_to=plot_crs  )
+     
+    rg = NULL
+    if (!is.null(area_lines )) {
+      rg = aegis.polygons::area_lines.db( DS=area_lines, returntype="sf", project_to=plot_crs )
+    }
+
+    z = NULL
+    if (!is.null(isobaths)) {
+      z = aegis.bathymetry::isobath_db( depths=isobaths, project_to=plot_crs )
+    }
+
     rg = aegis.polygons::area_lines.db( DS="cfa.regions", returntype="sf", project_to=plot_crs )
-    cl = st_transform( polygons_rnaturalearth(), st_crs(plot_crs) )
-
-    require(ggplot2)
-    require(tmap)
-
+    
+    cl = NULL
+    if (!is.null(coastline)) {
+      # coastline =  c("unsited states of america", "canada")
+      cl = st_transform( polygons_rnaturalearth(countries=coastline, xlim=xlim, ylim=ylim), st_crs(plot_crs) )
+    }
+ 
     O = list()
-    O[["tmap"]] =  
-      tm_shape( z,  projection=plot_crs ) + tm_lines( col="slategray", alpha=0.5, lwd=0.2) +
-      tm_shape( rg, projection=plot_crs ) + tm_lines( col="slategray", alpha=0.75, lwd=2)   + 
-      tm_shape( cl, projection=plot_crs ) + tm_borders( col = "slategray", alpha=0.5, lwd=0.5)
 
-    O[["ggplot2"]] =  ggplot() +
-      geom_sf( data=z,  fill=NA, col = "slategray",  lwd=0.25) +
-      geom_sf( data=rg, fill=NA, col = "slategray",  lwd=2.0) + 
-      geom_sf( data=cl, fill=NA, col = "slategray", lwd=0.5)
+    if ("ggplot" %in% target) {
+      require(ggplot2)
 
+      O[["ggplot"]] =  ggplot() +
+        geom_sf( data=z,  fill=NA, col = "slategray",  lwd=0.25) +
+        geom_sf( data=rg, fill=NA, col = "slategray",  lwd=2.0) + 
+        geom_sf( data=cl, fill=NA, col = "slategray", lwd=0.5)
+    }
+
+    if ("tmap" %in% target) {
+      require(tmap)
+
+      O[["tmap"]] =  
+        tm_shape( z,  projection=plot_crs ) + tm_lines( col="slategray", alpha=0.5, lwd=0.2) +
+        tm_shape( rg, projection=plot_crs ) + tm_lines( col="slategray", alpha=0.75, lwd=2)   + 
+        tm_shape( cl, projection=plot_crs ) + tm_borders( col = "slategray", alpha=0.5, lwd=0.5)
+    }
 
     dir.create( p$project.outputdir, showWarnings = FALSE, recursive = TRUE )
     saveRDS( O, file=fn, compress=TRUE )
