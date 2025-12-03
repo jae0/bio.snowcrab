6ca0db93236e85c12d73fe25c981ef483c32883f
jae0 <2574393+jae0@users.noreply.github.com>
2025-12-03 19:37:11 -0400

    another data.tab le relic

--- R/empirical.ranges.r
+++ R/empirical.ranges.r
@@ -1,7 +1,9 @@
 
   empirical.ranges = function( db="snowcrab", var, remove.zeros=F, probs=c(0.025, 0.975) ) {
     if (db =="snowcrab") {
-      x = snowcrab.db("set.complete")[, var]  
+      x = snowcrab.db("set.complete")
+      setDT(x)
+      x = x[[var]]
       if (remove.zeros) x = x[ which( x>0 ) ]
       return ( quantile( x, probs=probs, na.rm=T ) )
     }    
