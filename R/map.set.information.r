d64d87ed94e8f4f97864d36c7206e061743d98d0
jae0 <2574393+jae0@users.noreply.github.com>
2025-12-07 18:26:00 -0400

    MBA seems broken reverting to TPS for mapping

--- R/map.set.information.r
+++ R/map.set.information.r
@@ -44,7 +44,7 @@ map.set.information = function(p, outdir, variables, mapyears,
 
           if(nrow(set_xyz)<minN)next() #skip to next variable if not enough data
 
-          offset = empirical.ranges( db="snowcrab", v, remove.zeros=TRUE , probs=0)  # offset fot log transformation
+          offset = empirical.ranges( db="snowcrab", v, remove.zeros=TRUE , probs=0)  # offset for log transformation
           er = empirical.ranges( db="snowcrab", v, remove.zeros=TRUE , probs=probs)  # range of all years
           if (ratio) {
             er=c(0,1)
@@ -55,43 +55,48 @@ map.set.information = function(p, outdir, variables, mapyears,
 
           ler = er
           
-          if (length(withdata) < 3) print(paste("skipped",v, y, "<3 data points to create map", sep="."))
-          if (length(withdata) < 3) next()
-          vns2 = c("plon", "plat")
-          S = set_xyz[ withdata, ..vns2 ]
+          if (length(withdata) < 3) {
+            print(paste("skipped",v, y, "<3 data points to create map", sep="."))
+            next()
+          }
 
-          distances =  rdist( predlocs[ aoi, c("plon", "plat")], S)
+
+          distances =  rdist( 
+            predlocs[ aoi, .(plon, plat)], 
+            set_xyz[ withdata, .(plon, plat) ]
+          )
+
           distances[ which(distances < ptheta) ] = NA
           shortrange = which( !is.finite( rowSums(distances) ) )
           ips = aoi[ shortrange ]
           
           if (log.variable){
             set_xyz$z = log10(set_xyz$z+offset)
-            ler=log10(er+offset)
-            #if(offset<1)if(shift) xyz$z = xyz$z + abs(log10(offset))
+            ler=log10(er+offset) 
           }
 
           datarange = seq( ler[1], ler[2], length.out=50)
-          xyzi = na.omit(set_xyz)
+          set_xyz = na.omit(set_xyz)
 
-          if (nrow(xyzi) < minN || is.na(er[1])) next() #skip to next variable if not enough data
+          if (nrow(set_xyz) < minN || is.na(er[1])) next() #skip to next variable if not enough data
   
 
           if(interpolate.method=='mba'){
             # seems suspect .. 
-            u= MBA::mba.surf(x=xyzi[,.(plon,plat,z)], nplon, nplat, sp=TRUE, extend=TRUE   )
-            xyz = cbind( predlocs[ips, 1:2], u$xyz.est@data$z[ips] )
+            u= MBA::mba.surf(x=set_xyz[,.(plon,plat,z)], nplon, nplat, sp=TRUE, extend=TRUE   )
+            pred_xyz = cbind( predlocs[ips, 1:2], u$xyz.est@data$z[ips] )
           }
         
           if(interpolate.method=='tps'){
             # broken ?
-            u= fastTps(x=xyzi[,.(plon,plat)] , Y=xyzi[["z"]], theta=theta )
-            xyz = cbind( predlocs[ips, 1:2], predict(u, xnew=predlocs[ips, 1:2]))
+            u= fastTps(x=set_xyz[,.(plon,plat)] , Y=set_xyz[["z"]], theta=theta )
+            xyz = cbind( predlocs[ips, 1:2], predict(u, xnew=predlocs[ips,1:2]))
           }
+
           if(interpolate.method=='idw'){
             # broken?
             require(gstat)
-            u = gstat(id = "z", formula = z ~ 1, locations = ~ plon + plat, data = xyzi, set = list(idp = idp))
+            u = gstat(id = "z", formula = z ~ 1, locations = ~ plon + plat, data = set_xyz, set = list(idp = idp))
             xyz = predict(u, predlocs[ips, 1:2])[,1:3]
           }
           
@@ -101,10 +106,10 @@ map.set.information = function(p, outdir, variables, mapyears,
           cols = colorRampPalette(c("darkblue","cyan","green", "yellow", "orange","darkred", "black"), space = "Lab")
 
           xyz$z[xyz$z>ler[2]] = ler[2]
-          if(ratio)xyz$z[xyz$z<ler[1]] = ler[1]
+          if (ratio) xyz$z[xyz$z<ler[1]] = ler[1]
 
           ckey=NULL
-          if(log.variable){
+          if (log.variable){
             # create labels for legend on the real scale
             labs=as.vector(c(1,2,5)%o%10^(-4:5))
             labs=labs[which(labs>er[1]&labs<er[2])]
@@ -117,7 +122,7 @@ map.set.information = function(p, outdir, variables, mapyears,
           print(filename)
           png( filename=filename, width=3072, height=2304, pointsize=40, res=300 )
           lp = aegis_map( xyz, xyz.coords="planar", depthcontours=TRUE, 
-            pts=set_xyz[,c("plon","plat")],
+            pts=set_xyz[,.(plon,plat)],
             annot=y, annot.cex=4, at=datarange, col.regions=cols(length(datarange)+1),
             colpts=FALSE, corners=p$corners, display=FALSE, colorkey=ckey, plotlines="cfa.regions" )
           print(lp)
